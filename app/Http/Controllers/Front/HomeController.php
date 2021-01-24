c2011aa5bcbe6e28465a84acdf8b856f6027821a
rickymhzn <maharjanricky@gmail.com>
2021-01-24 15:09:03 +0545

    Frontend

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use App\Models\Country;
+use App\Models\University;
+use App\Models\Team;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $sliders = Slider::orderBy('id','asc')->get();
-        return view('front.index',compact('sliders'));
+        $countries = Country::orderBy('id','asc')->limit(6)->get();
+        $universities = University::orderBy('id','asc')->limit(6)->get();
+        $teams = Team::orderBy('id','asc')->get();
+        return view('front.index',compact('sliders','countries','universities','teams'));
     }
 }
--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,22 +1,84 @@
 <footer>
-<div class="top-footer">
-<div class="container">
-        <div class="row">
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4">
-            </div>
+    <div class="important-link" style="background-image: url('');">
+      <div class="row">
+        <div class="col-md-3">
+          <h3>Head office</h3>
+          <ul>
+            <li>
+              <i class="fa fa-map-marker"></i>
+              <p>Koteshor Kathmandu, Near Bhatbhateni</p>
+            </li>
+            <li>
+              <i class="fa fa-phone"></i>
+              <p>01-5100315 / 01-5100527</p>
+            </li>
+            <li>
+              <i class="fa fa-whatsapp"></i>
+              <p>9801100761</p>
+            </li>
+            <li>
+              <i class="fa fa-envelope"></i>
+              <p>info@cosmioinfotech.com</p>
+            </li>
+          </ul>
+          <h4 class="branch-location">
+            <a href="#" style="text-decoration: none;"> Our Branch Offices</a>
+          </h4>
         </div>
+        <div class="col-md-3">
+          <h3>Quick Link</h3>
+          <ul>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>Home</p>
+              </a>
+            </li>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>About</p>
+              </a>
+            </li>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>Our Services</p>
+              </a>
+            </li>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>Study Abroad</p>
+              </a>
+            </li>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>Our Courses</p>
+              </a>
+            </li>
+            <li>
+              <i class="fa fa-check"></i>
+              <a href="./index.html">
+                <p>Scholarship</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h3>Find Us Here</h3>
+          <div style="width: 100%; background-color: #fff; padding: 10px;">
+            <iframe style="padding:10px" width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=cosmioinfotech+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            </iframe>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <h3>our facebook page</h3>
+          <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v9.0" nonce="jXGqZTLw"></script>
+            <div class="fb-page" data-href="https://www.facebook.com/cosmio.infotech.92" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+        </div>
+      </div>
     </div>
-</div>
-</div class="bottom-footer">
-<strong>Copyright &copy; 2020 <a href="http://cosmioinfotech.com/" target="_blank">Cosmio Infotech</a>.</strong>
-     All rights reserved.
-     <div class="float-right d-none d-sm-inline-block">
-      Designed And Developed By <a href="http://cosmioinfotech.com/" target="_blank">Cosmio Infotech</a> | Privacy Policy
-     </div>
-</div>
-    
 </footer>
